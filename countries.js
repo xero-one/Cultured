3e35d4eb1623f7f0c1be7285251923d1ee4683de
Varun Master <varunmaster95@gmail.com>
2019-10-10 13:27:03 -0400

    added js files for countries info and news articles info

---
+++ countries.js
@@ -0,0 +1,14 @@
+var userQury = "GRC"; //$("#user-query").val();
+
+$.ajax({
+    url: "https://restcountries.eu/rest/v2/alpha/" + userQury,
+    method: "GET"
+}).then(function(res){
+    console.log(res);
+    $("#countries").append(res.flag,"<br>");
+    $("#countries").append(res.capital,"<br>");
+    $("#countries").append(res.currencies[0].code,"<br>");
+    $("#countries").append(res.languages[0].name,"<br>");
+    $("#countries").append(res.population,"<br>");
+    $("#countries").append(res.timezones[0],"<br>");
+});
---
+++ news.js
@@ -0,0 +1,16 @@
+var userQuery = "china";//$("#user-query").val();
+
+$.ajax({
+    url: "https://newsapi.org/v2/top-headlines?q="+userQuery+"&apiKey=fb3a00f195804d52a07c98c270c813ad&pageSize=5",
+    method: "GET"
+}).then(function(res){
+    console.log(res);
+    var articles = res.articles;
+    for(var i = 0; i < articles.length; i++){
+        $("#news").append(articles[i].title,"<br>");
+        $("#news").append(articles[i].url,"<br>");
+        console.log(articles[i].title);
+        console.log(articles[i].url);
+    }
+});
+
