75d55006c0d0e82ddc624a870b76e0f6f140fa1c
Varun Master <varunmaster95@gmail.com>
2019-10-11 12:32:10 -0400

    added dropdown and updated the news and country api to update based on user selection

--- assets/javascript/countries.js
+++ assets/javascript/countries.js
@@ -1,17 +1,19 @@
-var userQury = $(".country-name").text();
+$("#country-name").change(function () {
+    var userQury = $("#country-name option:selected").val();
 
-$.ajax({
-    url: "https://restcountries.eu/rest/v2/alpha/" + userQury,
-    method: "GET"
-}).then(function(res){
-    console.log(res);
-    var img = $("<img>");
-    img.attr("src",res.flag);
-    img.attr("style", "width: 80px; height: 30px;");
-    $(".countries").append(img,"<br>");
-    $(".countries").append("<i>Capital of the country: </i><strong>" + res.capital,"</strong><br>");
-    $(".countries").append("<i>Currency used: </i><strong>" + res.currencies[0].code,"</strong><br>");
-    $(".countries").append("<i>Language spoken: </i><strong>" + res.languages[0].name,"</strong><br>");
-    $(".countries").append("<i>Current population: </i><strong>" + (res.population).toLocaleString(),"</strong><br>");
-    $(".countries").append("<i>Country timezone: </i><strong>" + res.timezones[0],"</strong><br>");
+    $.ajax({
+        url: "https://restcountries.eu/rest/v2/alpha/" + userQury,
+        method: "GET"
+    }).then(function (res) {
+        console.log(res);
+        var img = $("<img>");
+        img.attr("src", res.flag);
+        img.attr("style", "width: 80px; height: 30px;");
+        $(".countries").append(img, "<br>");
+        $(".countries").append("<i>Capital of the country: </i><strong>" + res.capital, "</strong><br>");
+        $(".countries").append("<i>Currency used: </i><strong>" + res.currencies[0].code, "</strong><br>");
+        $(".countries").append("<i>Language spoken: </i><strong>" + res.languages[0].name, "</strong><br>");
+        $(".countries").append("<i>Current population: </i><strong>" + (res.population).toLocaleString(), "</strong><br>");
+        $(".countries").append("<i>Country timezone: </i><strong>" + res.timezones[0], "</strong><br>");
+    });
 });
--- assets/javascript/news.js
+++ assets/javascript/news.js
@@ -1,29 +1,34 @@
-var userQuery = $(".country-name").val();
-console.log("hello:",userQuery);
-
-$.ajax({
-    url: "https://newsapi.org/v2/top-headlines?q=" + userQuery + "&apiKey=fb3a00f195804d52a07c98c270c813ad&pageSize=5",
-    method: "GET"
-}).then(function (res) {
-    console.log(res);
-    var articles = res.articles;
-    for (var i = 0; i < articles.length; i++) {
-        var news = $("<li>");
-
-        var link = $("<a>");
-        link.attr("href", articles[i].url);
-        link.attr("target", "_blank");
-        news.append(link);
-        link.text(articles[i].title);
-
-        // $(".news").append(articles[i].title,"<br>");
-        // $(".news").append(articles[i].url,"<br>");
-        // console.log(articles[i].title);
-        // console.log(articles[i].url);
-        news.append(link);
-        $(".news-articles").append(news);
-    }
+$("#country-name").change(function () {
+    var userQuery = $("#country-name option:selected").val();
+    //$('input[name="question"]:checked').val();
+    // console.log("hello:", userQuery);
+    $.ajax({
+        url: "https://newsapi.org/v2/top-headlines?q=" + userQuery + "&apiKey=fb3a00f195804d52a07c98c270c813ad&pageSize=5",
+        method: "GET"
+    }).then(function (res) {
+        console.log(res);
+        var articles = res.articles;
+        for (var i = 0; i < articles.length; i++) {
+            var news = $("<li>");
+    
+            var link = $("<a>");
+            link.attr("href", articles[i].url);
+            link.attr("target", "_blank");
+            news.append(link);
+            link.text(articles[i].title);
+    
+            // $(".news").append(articles[i].title,"<br>");
+            // $(".news").append(articles[i].url,"<br>");
+            // console.log(articles[i].title);
+            // console.log(articles[i].url);
+            news.append(link);
+            $(".news-articles").append(news);
+        }
+    });
 });
+
+
+
 
 var countryName = [{
     "name": "Afghanistan",
